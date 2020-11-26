24c30712c5c78067e27de608a0ff03eba2544c5e
kally95 <kally-95@hotmail.com>
2020-11-26 00:40:32 +0000

    Added methods to threadstore

--- goreddit.go
+++ goreddit.go
@@ -9,11 +9,26 @@ type Thread struct {
 	Description string    `db:"description"`
 }
 
-// Post = What a thread is made up of
+// Post = what a thread is made up of
 type Post struct {
 	ID       uuid.UUID `db:"id"`
 	ThreadID uuid.UUID `db:"thread_id"`
 	Title    string    `db:"title"`
 	Content  string    `db:"content"`
 	Votes    int       `db:"votes"`
+}
+
+type Comment struct {
+	ID      uuid.UUID `db:"id"`
+	PostID  uuid.UUID `db:"post_id"`
+	Content string    `db:"content"`
+	Votes   int       `db:"votes"`
+}
+
+type ThreadStore interface {
+	Thread(id uuid.UUID) (Thread, error)
+	Threads() ([]Thread, error)
+	CreateThread(t *Thread) error
+	UpdateThread(t *Thread) error
+	DeleteThread(id uuid.UUID) error
 }
